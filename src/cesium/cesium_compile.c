bceb083e0a9052e287a7cbb113a0934390886fe0
Xeno333 <Xeno333@mailfence.com>
2024-06-24 13:52:55 -0500

    commit

--- src/cesium/cesium_compile.c
+++ src/cesium/cesium_compile.c
@@ -9,6 +9,43 @@
 #include "cesium.h"
 
 
+
+
+
+static char sep_chars[] = ":,. |\\~`!@#$%%^&*()-_=+[{}];\"'<>?/";
+
+
+static bool contains(char* str, const char c) {
+	for (size_t p = 0; str[p] != 0; p++) {
+		if (str[p] == c)
+			return true;
+	}
+	return false;
+}
+
+
+static char* get_part(char* in, size_t p) {
+	size_t c = 0;
+	do {
+		c++;
+	} while ((in[p+c] != 0) && (!contains(sep_chars, in[p+c])));
+
+	char* s = malloc(c + 1);
+	
+	if (c > 0)
+		memcpy(s, in+p, c);
+
+	s[c] = 0;
+
+	return s;
+}
+
+
+
+
+
+
+
 int compile_file(const compiler_args_t args) {
 	//return code
 	int rc = 0;
@@ -24,22 +61,26 @@ int compile_file(const compiler_args_t args) {
 
 	char* fin = fget(parse_fn);
 
-	/*long long p = 0L;
-	bool esc = false;
-	long long block_depth = 0L;
-	enum {no, line, multi} comment = no;
-	while (fin[p] != '\0') {
-		while (((fin[p] != '\n') && (esc != true)) && (fin[p] != '\0') && (fin[p] != ';')) {
-			p++;
-		}
-		if (comment == line) {
-			comment = no;
-		}
-		if (fin[p] == '\0') {
-			break;
-		}
-	}*/
 
+	size_t p = 0;
+	char* part = NULL;
+	while(true) {
+		if (part != NULL) 
+			free(part);
+			
+		part = get_part(fin, p);
+		if (part == NULL){
+			rc = 1;
+			goto done;
+		}
+
+		if (part[0] == 0) 
+			break;
+
+		p += strlen(part);
+
+		printf("%s ", part);
+	}
 
     done:
 		if (parse_fn != NULL) {
